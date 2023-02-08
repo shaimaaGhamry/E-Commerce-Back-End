2856ba5bb6f3ac9c9067e311b69fa920357218de
unknown <shimaa_elghamry@hotmail.com>
2023-02-08 13:02:30 -0400

    populate the database and the relations betweeen the tables

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,8 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      dialect: 'mysql',
+      dialect: 'mysql',      
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
 // import models
-const Product = require('./Product');
 const Category = require('./Category');
+
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category,
+  {foreignKey: 'category_id'} );
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:'category_id',
+  onDelete:'cascade'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag,
+    unique:false
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through:{
+    model : ProductTag,
+    unique:false
+  }
+})
 
 module.exports = {
   Product,
